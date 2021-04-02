56bc87cad5c3168e7f637d3f69f942bce7ba4595
João Divino <joaodivinod@gmail.com>
2021-04-02 17:23:56 -0300

    Stock criado e primeira validação realizada

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,10 +1,47 @@
 <template>
-    <h1>Ação</h1>
+    <v-flex class="pr-3 mb-3 xs12 md6 lg4">
+        <v-card class="green darken-3 white--text">
+            <v-card-title class="headline">
+                <strong>{{ stock.name }} </strong> <small> (Preço:{{stock.price}})</small>
+            </v-card-title>
+        </v-card>
+        <v-card>
+            <v-container fill-height>
+                <v-text-field label="Quantidade" type="number" v-model.number="quantity"></v-text-field>
+                <v-btn :disabled="quantity<=0 || !Number.isInteger(quantity)"  class="green darken-3 white--text"
+                @click="buyStock"
+                >Comprar</v-btn>
+            </v-container>
+            <p v-show="quantity<0 || !Number.isInteger(quantity) ">Por favor insira um valor válido</p>
+        </v-card>
+
+    </v-flex>
 </template>
 
 <script>
 export default {
-name: "Stock"
+    name: "Stock",
+    props:['stock'],
+    data(){
+        return{
+            quantity:0
+        }
+    },
+    methods:{
+        buyStock(){
+            const order = {
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            }
+
+            console.log(order)
+            this.quantity = 0
+
+        }
+
+    }
+
 }
 </script>
 
